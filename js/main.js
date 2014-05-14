a2026b146160502bf15cf404d581ce947376b81c
KostyaD <thedamaxstudio@gmail.com>
2014-05-14 12:37:33 +0400

    work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).on('click touchstart', '.card', function(){
+	cards.Click($(this));
+});
 
+var cards = {
+	i: 0,
+	clicked: false,
+	Click: function(card) {
+		var obj = this;
+		if(obj.i < 2)
+		{
+			card.addClass('flip');
+			obj.i++;
+		}
+		if(obj.i == 2)
+		{
+			setTimeout(function(){
+				$('.card').removeClass('flip');
+				obj.i = 0;
+			}, 1000);
+		}
+	}
+};
